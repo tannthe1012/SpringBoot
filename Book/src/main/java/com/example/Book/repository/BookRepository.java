4e0f1de788ebe5589c168196292b0097c9744368
The Tan <cutehkt2x@gmail.com>
2020-09-26 19:42:53 +0700

    update lan 2

--- Book/src/main/java/com/example/Book/repository/BookRepository.java
+++ Book/src/main/java/com/example/Book/repository/BookRepository.java
@@ -3,7 +3,9 @@ package com.example.Book.repository;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
+
+import com.example.Book.entity.Book1;
 @Repository
 public interface BookRepository {
-	public List dataBookRepository();
+	public List<Book1> dataBookRepository();
 }
